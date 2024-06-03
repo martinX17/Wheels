5e33f577811a6de6dcd01436f9b621b7a77b3716
martinX17 <yakubmaniyar1709@gmail.com>
2024-06-04 04:27:53 +0530

    Started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <div className="">
+      Hello World!
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main-yellow': '#f5b754',
+      },
+    },
+  },
+  plugins: [],
+}
+
